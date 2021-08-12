ffd39a3b19c238c01d8d369e9cfc503aa404314b
LaurenceLungo <pnng@connect.ust.hk>
2021-08-12 23:32:10 +0800

    - add 2-way binding to station selection menu

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -7,18 +7,24 @@ import { useState } from "react";
 function Footer(props) {
 
     const [currentStation, setCurrentStation] = useState(null);
+    const [selectedStation, setSelectedStation] = useState("unselected");
 
-    function callback(e) {
+    function stationMenuCallback(e) {
         setCurrentStation(e.currentTarget.value);
         props.callback(e.currentTarget.value);
+    }
+
+    function favStationCallback(station) {
+        setSelectedStation(station);
+        props.callback(station);
     }
 
     return (
         <Box className={classes.footer}>
             <Circle>
-                <StationMenu callback={callback} selected={props.selected} />
+                <StationMenu callback={stationMenuCallback} selected={selectedStation} />
             </Circle>
-            <FavStations currentStation={currentStation} callback={props.callback} />
+            <FavStations currentStation={currentStation} callback={favStationCallback} />
         </Box>
     )
 }
--- src/components/footer/stationMenu/stationMenu.js
+++ src/components/footer/stationMenu/stationMenu.js
@@ -1,11 +1,24 @@
 import classes from "./stationMenu.module.css";
-import { Select } from "@chakra-ui/react"
+import { Select, useProps } from "@chakra-ui/react"
 import config from '../../../config';
+import { useEffect, useState } from "react";
 
 function StationMenu(props) {
+
+    const[selected, setSelected] = useState(props.selected);
+
+    function onChangeHandler(e) {
+        setSelected(e.currentTarget.value);
+        props.callback(e);
+    }
+
+    useEffect(() => {
+        setSelected(props.selected)
+    }, [props.selected])
+
     return (
         <div className={classes.select}>
-            <Select onChange={props.callback} defaultValue="unselected">
+            <Select onChange={onChangeHandler} value={selected}>
                 <option key="unselected" value="unselected" disabled>- 請選擇 Please Select -</option>
                 {Object.keys(config.stationName).map(station => {
                     return <option key={station} value={station}>{config.stationName[station]['name']}</option>
