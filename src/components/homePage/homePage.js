e39192b2b68e006da0a4451fbd4ba0766c8be45d
LaurenceLungo <npn5000@gmail.com>
2022-08-13 02:26:05 +0800

    remove unused import

--- src/components/homePage/homePage.js
+++ src/components/homePage/homePage.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import classes from "./homePage.module.css";
 import Footer from "../footer/footer";
 import PlatformCard from "../platformCard/platformCard";
