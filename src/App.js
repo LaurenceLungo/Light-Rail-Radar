9036f6f4e85a03a8805c57da7282f1ed464d23a3
Laurence Ng <npn5000@gmail.com>
2025-07-12 12:38:22 +0800

    Implement scroll-based opacity effect for LanguageSelector

--- src/App.js
+++ src/App.js
@@ -1,36 +1,10 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import './App.css';
 import HomePage from './components/homePage/homePage';
-import { ChakraProvider, Button, HStack, Box } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import theme from "./theme";
-import { LanguageProvider, LanguageContext } from './context/LanguageContext';
-
-const LanguageSelector = () => {
-  const { language, setLanguage } = useContext(LanguageContext);
-  
-  return (
-    <Box position="fixed" top="4" right="4" zIndex="999">
-      <HStack spacing={1}>
-        <Button 
-          size="sm" 
-          colorScheme="blue"
-          variant={language === 'en' ? 'solid' : 'outline'}
-          onClick={() => setLanguage('en')}
-        >
-          Eng
-        </Button>
-        <Button 
-          size="sm"
-          colorScheme="blue"
-          variant={language === 'zh' ? 'solid' : 'outline'} 
-          onClick={() => setLanguage('zh')}
-        >
-          中文
-        </Button>
-      </HStack>
-    </Box>
-  );
-};
+import { LanguageProvider } from './context/LanguageContext';
+import LanguageSelector from './components/languageSelector/languageSelector';
 
 function App() {
   return (
